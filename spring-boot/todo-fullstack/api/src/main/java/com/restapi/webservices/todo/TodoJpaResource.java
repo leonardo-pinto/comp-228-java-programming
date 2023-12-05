934f298405e6a23aee5fb1e02b8474592527a930
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-12-05 11:08:22 -0500

    add section 13: add jpa and hibernate to todo full stack app

--- spring-boot/todo-fullstack/api/src/main/java/com/restapi/webservices/todo/TodoJpaResource.java
+++ spring-boot/todo-fullstack/api/src/main/java/com/restapi/webservices/todo/TodoJpaResource.java
@@ -4,41 +4,46 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
-//@RestController
+@RestController
 @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
-public class TodoController {
+public class TodoJpaResource {
 
-    private TodoService service;
+    private TodoRepository repository;
 
-    public TodoController(TodoService service) {
-        this.service = service;
+    public TodoJpaResource(TodoRepository repository) {
+        this.repository = repository;
     }
 
     @GetMapping("/users/{username}/todos")
     public List<Todo> retrieveTodos(@PathVariable String username){
-        return service.findByUsername(username);
+
+        return repository.findByUsername(username);
     }
 
     @GetMapping("/users/{username}/todos/{id}")
     public Todo retrieveTodo(@PathVariable String username, @PathVariable int id){
-        return service.findById(id);
+
+        return repository.findById(id).get();
     }
 
     @DeleteMapping("/users/{username}/todos/{id}")
     public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable int id) {
-        service.deleteById(id);
+        repository.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/users/{username}/todos/{id}")
     public Todo updateTodo(@PathVariable String username, @PathVariable int id, @RequestBody Todo todo) {
-        service.updateTodo(todo);
+        repository.save(todo);
         return todo;
     }
 
     @PostMapping("/users/{username}/todos")
     public Todo createTodo(@PathVariable String username, @RequestBody Todo todo) {
-        return service.addTodo(username, todo.getDescription(), todo.getTargetDate(), todo.isDone());
+        todo.setUsername(username);
+        todo.setId(null);
+        return repository.save(todo);
     }
 }
