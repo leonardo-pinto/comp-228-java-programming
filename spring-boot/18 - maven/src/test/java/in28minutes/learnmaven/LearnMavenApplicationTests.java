0b730754311b7c16a0bae2910904012dc7a49ea9
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-12-12 09:22:40 -0500

    add section 18: maven

---
+++ spring-boot/18 - maven/src/test/java/in28minutes/learnmaven/LearnMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package in28minutes.learnmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
