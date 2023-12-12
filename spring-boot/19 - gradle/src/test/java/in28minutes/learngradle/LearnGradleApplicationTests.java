feb8dca32faf22aba8ebff312dd048136fda6d30
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-12-12 10:01:09 -0500

    add section 19: gradle

---
+++ spring-boot/19 - gradle/src/test/java/in28minutes/learngradle/LearnGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package in28minutes.learngradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
